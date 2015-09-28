671f3cafd8926f0a8f7fcd37a153893d6cea0052
Ankur Kumar <pro.ankurk1@gmail.com>
2015-09-28 15:35:28 +0530

    os detection for gulp release tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
 
 var gulp = require('gulp'),
     gutil = require("gulp-util"),
+    utils = require('./tasks/utility'),
     del = require('del');
 
 gulp.task('cleanAll', function () {
-    del(['./dist','./tmp','./release','./build','./npm-debug.log'], {force: true}).then(function(paths){
+    del(['./dist', './tmp', './release', './build', './npm-debug.log'], {force: true}).then(function (paths) {
         gutil.log('Success :', gutil.colors.green('Cleaning completed.'));
     });
 });
@@ -13,6 +14,12 @@ gulp.task('cleanAll', function () {
 require('./tasks/dist.js');
 require('./tasks/build.js');
 
-require('./tasks/release_linux_64.js');
-require('./tasks/release_osx_64.js');
-require('./tasks/release_windows_32.js');
+if (utils.os === 'linux') {
+    require('./tasks/release_linux_64.js');
+} else if (utils.os === 'windows') {
+    require('./tasks/release_windows_32.js');
+} else if (utils.os === 'osx') {
+    require('./tasks/release_osx_64.js');
+
+}
+
